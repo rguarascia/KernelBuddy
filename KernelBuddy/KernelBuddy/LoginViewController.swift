bbd62c23e99ba443de0b39465a7d5f0dcc430ce3
Ryan Guarascia <rguarascia@gmail.com>
2018-12-17 21:28:18 -0500

    Initial Log in and create.

---
+++ KernelBuddy/KernelBuddy/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  KernelBuddy
+//
+//  Created by Ryan Guarascia on 2018-12-17.
+//  Copyright © 2018 Ryan Guarascia. All rights reserved.
+//
+
+import UIKit
+
+// Handles the logging in of exisiting customers
+class LoginViewController : UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func login(_ sender: Any) {
+        
+        //resets
+        email.backgroundColor = UIColor.white
+        password.backgroundColor = UIColor.white
+        
+        let incorrectColor = UIColor.init(displayP3Red: 207.0, green: 75.0, blue: 131.0, alpha: 0.5)
+        
+        // checks for blanks and validation
+        if(email.text?.isEmpty ?? false) {
+            email.backgroundColor = incorrectColor
+        }
+        if (password.text?.isEmpty ?? false) {
+            password.backgroundColor = incorrectColor
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    
+    
+}
---
+++ KernelBuddy/KernelBuddy/RegisterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterViewController.swift
+//  KernelBuddy
+//
+//  Created by Ryan Guarascia on 2018-12-17.
+//  Copyright © 2018 Ryan Guarascia. All rights reserved.
+//
+
+import UIKit
+
+// handles when we want to create a new user
+class RegisterViewController : UIViewController {
+    
+}
